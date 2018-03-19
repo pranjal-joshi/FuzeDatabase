11f26af2c2f9b9055d7387797f539d9c58a6cf25
Pranjal Joshi <joshi.pranjal5@gmail.com>
2018-03-19 10:20:09 +0530

    bkp filename changed

--- db_bkp.bat
+++ db_bkp.bat
@@ -12,20 +12,20 @@ ECHO ""
 
 REM Export all databases into file C:\path\backup\databases.[year][month][day].sql
 ECHO Backing up FuzeDatabase...
-"C:\xampp\mysql\bin\mysqldump.exe" fuze_database --result-file="E:\FuzeDatabase_backups\databases.%TIMESTAMP%.sql" --user="root" --password="fuzedbpass"
+"C:\xampp\mysql\bin\mysqldump.exe" fuze_database --result-file="E:\FuzeDatabase_backups\fuze_database.%TIMESTAMP%.sql" --user="root" --password="fuzedbpass"
 
 REM Change working directory to the location of the DB dump file.
 C:
 CD E:\FuzeDatabase_backups\
 
 REM Compress DB dump file into CAB file (use "EXPAND file.cab" to decompress).
-REM MAKECAB "databases.%TIMESTAMP%.sql" "databases.%TIMESTAMP%.sql.cab"
+REM MAKECAB "fuze_database.%TIMESTAMP%.sql" "fuze_database.%TIMESTAMP%.sql.cab"
 ECHO Compressing database file...
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Compress-Archive -Path 'E:\FuzeDatabase_backups\databases.%TIMESTAMP%.sql' -DestinationPath 'E:\FuzeDatabase_backups\databases.%TIMESTAMP%.zip' -Update"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Compress-Archive -Path 'E:\FuzeDatabase_backups\fuze_database.%TIMESTAMP%.sql' -DestinationPath 'E:\FuzeDatabase_backups\fuze_database.%TIMESTAMP%.zip' -Update"
 
 REM Delete uncompressed DB dump file.
 ECHO Deleting uncompressed database file...
-DEL /q /f "E:\FuzeDatabase_backups\databases.%TIMESTAMP%.sql"
+DEL /q /f "E:\FuzeDatabase_backups\fuze_database.%TIMESTAMP%.sql"
 
 ECHO -----------
 ECHO     DONE   
