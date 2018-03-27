4404d733a1593a03ff1a1728a779086b33e2b1e7
Pranjal Joshi <joshi.pranjal5@gmail.com>
2018-03-27 10:58:03 +0530

    query bug fix

--- after_pu_upload.php
+++ after_pu_upload.php
@@ -153,7 +153,7 @@
 				$sqlAutoIncReset = "ALTER TABLE `after_pu` DROP `_id`;";
 				$autoIncResult = mysqli_query($db, $sqlAutoIncReset);
 
-				$sqlAdd = "REPLACE INTO `after_pu` (`pcb_no`, `i`, `vee`, `vbat_pst`, `pst_amp`, `pst_wid`, `mod_freq`, `mod_dc`, `mod_ac`, `cap_charge`, `vrf_amp`, `vbat_vrf`, `vbat_sil`, `det_wid`, `det_amp`, `cycles`, `bpf_dc`, `bpf_ac`, `bpf_noise_ac`, `sil`, `lvp`, `pd_delay`, `pd_det`, `safe`, `result`, `op_name`) VALUES ";
+				$sqlAdd = "REPLACE INTO `after_pu` (`pcb_no`, `i`, `vee`, `vbat_pst`, `pst_amp`, `pst_wid`, `mod_freq`, `mod_dc`, `mod_ac`, `cap_charge`, `vrf_amp`, `vbat_vrf`, `vbat_sil`, `det_wid`, `det_amp`, `cycles`, `bpf_dc`, `bpf_ac`, `sil`, `lvp`, `pd_delay`, `pd_det`, `safe`, `result`, `op_name`, `bpf_noise_ac`) VALUES ";
 
 				foreach ($Reader as $Row)
 				{
