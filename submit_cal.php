43c3d0d89a45c57201eed615149547a885bb0906
Pranjal Joshi <joshi.pranjal5@gmail.com>
2018-08-27 17:51:40 +0530

    don't check if exist
    During cal process, partial result are saved first, and then after cal the remaining details are ti be filled. Thus REPLACE exisiting instead of denying entry.

--- submit_cal.php
+++ submit_cal.php
@@ -9,14 +9,32 @@
 
 		if($_POST['task'] == 'add') {
 
-			$sqlCheck = "SELECT * from `calibration_table` WHERE `pcb_no`='".$_POST['pcb_no']."';";
+			/*$sqlCheck = "SELECT * from `calibration_table` WHERE `pcb_no`='".$_POST['pcb_no']."';";
 			$checkResult = mysqli_query($db, $sqlCheck);
 
 			if($checkResult->num_rows > 0){
 				die("exist");
-			}
+			}*/
 
+			/*
 			$sql = "INSERT INTO `calibration_table` (`_id`, `pcb_no`, `rf_no`, `before_freq`, `before_bpf`, `changed`, `res_val`, `after_freq`, `after_bpf`, `timestamp`, `op_name`) VALUES (
+				NULL, 
+				'".$_POST['pcb_no']."', 
+				'".$_POST['rf_no']."', 
+				'".$_POST['before_freq']."', 
+				'".$_POST['before_bpf']."',
+				'".$_POST['changed']."', 
+				'".$_POST['resChange']."', 
+				'".$_POST['after_freq']."', 
+				'".$_POST['after_bpf']."', 
+				'".$_POST['datePicker']."', 
+				'".$_POST['op_name']."'
+					);";
+			*/
+
+			// Don't check if already exist.. Just replace!
+
+			$sql = "REPLACE INTO `calibration_table` (`_id`, `pcb_no`, `rf_no`, `before_freq`, `before_bpf`, `changed`, `res_val`, `after_freq`, `after_bpf`, `timestamp`, `op_name`) VALUES (
 				NULL, 
 				'".$_POST['pcb_no']."', 
 				'".$_POST['rf_no']."', 
